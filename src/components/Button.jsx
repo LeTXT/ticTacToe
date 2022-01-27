a29d2da5947492fba305b2b7f241e70245fb68c1
LeTXT <leandrogomesdeoliveira2@gmail.com>
2022-01-27 13:50:08 -0300

    2 files modified

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,20 +1,48 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 import './Button.css'
+import {arr, arrL, arrP} from './Arr'
+import Logic from './Logic'
 
 
 export default function Button(props) {
+  const [stateB, setStateB] = useState(false)
+
+ 
+const clearArr = () => {
+  // props.setState([arr[0]])
+  arrP[props.index] = arr[0]
+  arr === undefined? arrL[0] : arrL[0] = arr[0]
+  arr.shift()
+  
+  return arrP
+  }
+  
+  const xO = () => {arr[0] === 'x' ? 'red' : 'blue'; return 'red'}
+  
+  const disabled = () =>  setStateB(!stateB)
+
+  
+  // const index = (e) => arrP[e]
+  
+  const hadleClick = () => {
+    clearArr()
+    disabled()
+    Logic()
+    xO()
+    console.log('oi')
+  }
 
   return (
-    <div></div>
+    <div className='bodyButton' >
+      <div onClick={hadleClick()} disabled={stateB} className='btn'><h3 className={xO()} >{'x'}</h3></div>
+    </div>
   )
 }
 
 // export default function Button({stateB, onclick, classname, stateArr }) {
 
 //   return (
-//     <div className='bodyButton' >
-//       <div onClick={onclick} disabled={stateB} className='btn'><h3 className={classname} >{stateArr}</h3></div>
-//     </div>
+    
 //   )
 // }
