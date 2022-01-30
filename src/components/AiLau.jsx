0b99d77443bde37df1d21cbf6c077caf9d1569f1
LeTXT <leandrogomesdeoliveira2@gmail.com>
2022-01-30 20:55:28 -0300

    1 file modified

--- src/components/AiLau.jsx
+++ src/components/AiLau.jsx
@@ -30,15 +30,51 @@ export default function LauritaAi ({stateArr, setState, setStateAi, stateAi, set
     }
 
     const block = (a, b, c) => arrP[a] === arrP[b] && arrP[c] === undefined && arrP[a] !== undefined && arrP[b] !== undefined 
+
+    const aiGame = (a, b, c) => arrP[a] === arrP[b] && arrP[c] === undefined && arrP[a] !== undefined && arrP[b] !== undefined && arrP === 'o'
     
-    const single = (a, c) => arrP[a] !== undefined && arrP[c] === undefined
+    const aiSingle = (a, b, c) => arrP[a] === undefined && arrP[b] === undefined && arrP[c] !== undefined && arrP[c] === 'o'
     
     const test = () => {
                         
         if(arr !== undefined && arr[0] === 'o') {
             let m = math(0, 8)
-            
-            if(block(0, 1, 2)){
+            // Ia tenta ganhar aqui
+            if (aiGame(6, 8, 7)) {
+                run(7)
+            } else if (aiGame(6, 7, 8)) {
+                run(8)
+            } else if (aiGame(7, 8, 6)) {
+                run(6)
+            } else if (aiGame(6, 4, 2)) {
+                run(2)
+            } else if (aiGame(8, 4, 0)) {
+                run(0)
+            } else if (aiGame(7, 4, 1)) {
+                run(1)
+            } else if (aiGame(3, 4, 5)) {
+                run(5)
+            } else if (aiGame(5, 4, 3)) {
+                run(3)
+            } else if (aiGame(2, 4, 6)) {
+                run(6)
+            } else if (aiGame(0, 4, 8)) {
+                run(8)
+            } else if (aiGame(2, 1, 0)) {
+                run(0)
+            } else if (aiGame(2, 4, 7)) {
+                run(7)
+            } else if (aiGame(2, 0, 1)) {
+                run(1)
+            } else if (aiGame(0, 4, 2)) {
+                run(2)
+            }
+
+
+
+
+
+            else if(block(0, 1, 2)){
                 run(2)
             } else if(block(0, 2, 1)) {
                 run(1)
@@ -72,12 +108,16 @@ export default function LauritaAi ({stateArr, setState, setStateAi, stateAi, set
                 run(8)
             } else if(block(6, 8, 7)) {
                 run(7)
+            } else if(block(6, 4, 2)) {
+                run(2)
             } else if(block(2, 1, 0)) {
                 run(0)
             } else if(block(5, 4, 3)) {
                 run(3)
             } else if(block(8, 7, 6)) {
                 run(6)
+            } else if(block(8, 4, 0)) {
+                run(0)
             } else if(block(6, 3, 0)) {
                 run(0)
             } else if(block(7, 4, 1)) {
@@ -86,14 +126,21 @@ export default function LauritaAi ({stateArr, setState, setStateAi, stateAi, set
                 run(4)
             } 
             // Parte de jogadas em um único indice
-            else if (single(0, 4)) {
+            // else if (single(0, 4)) {
+            //     run(4)
+            // } else if (single(2, 4)) {
+            //     run(4)
+            // } else if (single(6, 4)) {
+            //     run(4)
+            // } else if (single(8, 4)) {
+            //     run(4)
+            // } 
+            else if ((arrP[0] !== undefined || arrP[2] !== undefined || arrP[6] !== undefined || arrP[8] !== undefined) && arrP[4] === undefined) {
                 run(4)
-            } else if (single(2, 4)) {
-                run(4)
-            } else if (single(6, 4)) {
-                run(4)
-            } else if (single(8, 4)) {
-                run(4)
+            }
+            // Estratégia mais complexas
+            else if (aiSingle(3, 5, 4)) {
+                run(3)
             }
              else if(arrP[m] === undefined) {
                 run(m)
